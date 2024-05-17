029d811973674f7e426e33c8abd54b0b38931e71
kunal goyal <engineerkunalgoyal@gmail.com>
2024-05-17 17:24:50 +0530

    updated mongodb

--- db/connection/index.js
+++ db/connection/index.js
@@ -2,19 +2,22 @@ import mongoose from 'mongoose';
 import 'dotenv/config';
 
 const { MONGO_CONNECTION_POOLSIZE, DATABASE_URL_DEV } = process.env;
+
 const options = {
   maxPoolSize: MONGO_CONNECTION_POOLSIZE || 20,
-  connectTimeoutMs: 5000,
+  connectTimeoutMS: 5000,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  tls: true,
 };
 
-const mongoose_connection = mongoose.createConnection(
-  DATABASE_URL_DEV,
-  options,
-);
+const mongoose_connection = mongoose.createConnection(DATABASE_URL_DEV, options);
+
 mongoose_connection.on('error', (error) => {
   console.error(`error on ${DATABASE_URL_DEV} Error \n${error.message}`);
-  throw error.message;
+  throw new Error(error.message);  // Proper error throwing
 });
+
 mongoose_connection.on('connecting', () => {
   console.info(`connecting -> ${DATABASE_URL_DEV}`);
 });
@@ -22,10 +25,13 @@ mongoose_connection.on('connecting', () => {
 mongoose_connection.on('uninitialized', () => {
   console.info(`uninitialized -> ${DATABASE_URL_DEV}`);
 });
+
 mongoose_connection.on('connected', () => {
   console.info(`connected to  -> ${DATABASE_URL_DEV}`);
 });
+
 mongoose_connection.on('disconnected', () => {
   console.error(`disconnected -> ${DATABASE_URL_DEV}`);
 });
+
 export default mongoose_connection;
