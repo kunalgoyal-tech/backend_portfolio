70fe129b36dca57a227d0077f025d4b892e893c3
kunal goyal <engineerkunalgoyal@gmail.com>
2024-05-17 17:39:09 +0530

    changes in cors

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ app.use(bodyParser.json());
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5000'
+}));
+
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
